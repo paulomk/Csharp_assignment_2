9de2ef37495ac2904fcf89ab886872a4ecf55836
PaulomK <paulo.massao.kawakami@gmail.com>
2024-06-17 17:32:04 -0400

    boiler plate

---
+++ 20240617_assignment2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _20240617_assignment2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
